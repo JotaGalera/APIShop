a1c27da0413c308e051c8e7b477d7050912e7167
Jota Galera <javigalera94@gmail.com>
2020-12-10 20:04:37 +0100

    Closed #16 get product detail added as new route

--- APIShopUI.py
+++ APIShopUI.py
@@ -15,41 +15,41 @@ def products():
         "list": [
             {
             "id": 1,
-            "name": "white-black trainers",
+            "name": "Originals",
             "brand": "Adidas",
-            "price": 80,
+            "price": 72,
             "currency": "€",
             "image": "https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas1.jpeg"
             },
             {
             "id": 2,
-            "name": "white-green trainers",
+            "name": "Stan Smith",
             "brand": "Adidas",
-            "price": 65,
+            "price": 55.25,
             "currency": "€",
             "image": "https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas2.jpeg"
             },
             {
             "id": 3,
-            "name": "black trainers",
+            "name": "Ultraboost 20",
             "brand": "Adidas",
-            "price": 70,
+            "price": 63,
             "currency": "€",
             "image": "https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas3.jpeg"
             },
             {
             "id": 4,
-            "name": "sport trainers",
+            "name": "Gazelle",
             "brand": "Adidas",
-            "price": 85,
+            "price": 80.75,
             "currency": "€",
             "image": "https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas4.jpeg"
             },
             {
             "id": 5,
-            "name": "white trainers",
+            "name": "Continental 80",
             "brand": "Adidas",
-            "price": 75,
+            "price": 72,
             "currency": "€",
             "image": "https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas5.jpeg"
             }
@@ -70,6 +70,93 @@ def images():
 def get_image(): 
     name = request.args.get("name")
     return send_from_directory(app.static_folder, filename=name, mimetype='image/jpg')
+
+@app.route('/products/details/', methods=['GET'])
+def get_product_detail(): 
+    product_id = request.args.get("product_id")
+
+    if product_id == "1":
+        return product_detail_one()
+    elif product_id == "2":
+        return product_detail_two()
+    elif product_id == "3":
+        return product_detail_three()
+    elif product_id == "4":
+        return product_detail_four()
+    elif product_id == "5":
+        return product_detail_five()
+    else:
+        return "Invalid product"
+
+def product_detail_one():
+    data = {
+            "id": 1,
+            "name": "Originals",
+            "color": "White & black",
+            "brand": "Adidas",
+            "original_price": 80,
+            "discount": 10,
+            "total_price": 72,
+            "currency": "€",
+            "images": ["https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas1detail1.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas1detail2.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas1detail3.jpg"]
+            }
+    return data
+
+def product_detail_two():
+    data = {
+            "id": 2,
+            "name": "Stan Smith",
+            "color": "White & green",
+            "brand": "Adidas",
+            "original_price": 65,
+            "discount": 15,
+            "total_price": 55.25,
+            "currency": "€",
+            "images": ["https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas2detail1.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas2detail2.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas2detail3.jpg"]
+            }
+    return data
+
+def product_detail_three():
+    data = {
+            "id": 3,
+            "name": "Ultraboost 20",
+            "color": "Black",
+            "brand": "Adidas",
+            "original_price": 70,
+            "discount": 10,
+            "total_price": 63,
+            "currency": "€",
+            "images": ["https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas3detail1.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas3detail2.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas3detail3.jpg"]
+            }
+    return data
+
+def product_detail_four():
+    data = {
+            "id": 4,
+            "name": "Gazelle",
+            "color": "Black",
+            "brand": "Adidas",
+            "original_price": 85,
+            "discount": 5,
+            "total_price": 80.75,
+            "currency": "€",
+            "image": ["https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas4detail1.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas4detail2.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas4detail3.jpg"]
+            }
+    return data
+
+def product_detail_five():
+    data = {
+            "id": 5,
+            "name": "Continental 80",
+            "color": "White",
+            "brand": "Adidas",
+            "original_price": 80,
+            "discount": 10,
+            "total_price": 72,
+            "currency": "€",
+            "image": ["https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas5detail1.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas5detail2.jpg","https://api-shop-ui.herokuapp.com/products/get_images/?name=adidas5detail3.jpg"]
+            }
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
